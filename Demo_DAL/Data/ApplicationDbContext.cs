3da33956f1e66001f485ef934d773df0d86c9578
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-03-22 23:24:22 +0200

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ Demo_DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Demo_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("server = . ; Database = MVCApplication ; Trusted_Connection = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Demo_DAL/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DAL.Models
+{
+    internal class Department
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+
+    }
+}
