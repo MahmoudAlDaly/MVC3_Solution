b0ddce738128e2e734bc15f3a82d517c6f8ad754
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-03-25 00:03:38 +0200

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ Demo_BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department Get(int id);
+        int Add(Department Entity);
+        int Update(Department Entity);
+        int Delete(Department Entity);
+    }
+}
---
+++ Demo_BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,64 @@
+﻿using Demo_BLL.Interfaces;
+using Demo_DAL.Data;
+using Demo_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext DbContext;
+
+
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+        public Department Get(int id)
+        {
+            //var department = DbContext.Departments.Local.Where(d => d.ID == id).FirstOrDefault();
+
+            //if (department == null)
+            //{
+            //    DbContext.Departments.Where(d => d.ID == id).FirstOrDefault();
+            //}
+
+            //return department;
+
+            return DbContext.Departments.Find(id);
+
+        }
+
+
+        public IEnumerable<Department> GetAll()
+        {
+            return DbContext.Departments.AsNoTracking().ToList();
+        }
+
+        public int Add(Department Entity)
+        {
+            DbContext.Departments.Add(Entity);
+            return DbContext.SaveChanges();
+        }
+
+        public int Update(Department Entity)
+        {
+            DbContext.Departments.Update(Entity);
+            return DbContext.SaveChanges();
+        }
+
+        public int Delete(Department Entity)
+        {
+            DbContext.Departments.Remove(Entity);
+            return DbContext.SaveChanges();
+        }
+
+
+        
+    }
+}
