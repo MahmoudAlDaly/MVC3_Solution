f922490f6bda52572d3c458683cd16e581b58bdb
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-04-07 22:55:34 +0200

    Part 01 Document Setting

---
+++ Demo_PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Demo_PL.Helpers
+{
+    public static class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file,string foldename)
+        {
+            // 1- get located folder path
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(),"WWWroot\\files",foldename);
+
+            if (Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // 2- get file name and make it unique
+            string fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+            // 3- get file path
+            string filePath = Path.Combine(folderPath,fileName);
+
+            // 4- save file as stremas [data pet time]
+            using FileStream fileStream = new FileStream(filePath,FileMode.Create);
+
+            file.CopyTo(fileStream);
+
+            return fileName;
+        }
+
+        public static void DeleteFile(string fileName,string folderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "WWWroot\\files", folderName,fileName);
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
